47b4a43f15f51311dfdf195cf811e71e055cd42c
jbfp <jakobp10@gmail.com>
2018-12-10 19:51:47 +0100

    cd to script location in test script

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-set -e
+
+set -e # break on error.
+cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" # cd to script location.
 ls ./test/**/*.csproj | xargs -L1 -P 0 dotnet test -c Release -v q
