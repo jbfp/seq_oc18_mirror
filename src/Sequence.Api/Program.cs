9abf6f61b003eb63c52664feefc826c6b9c0a1d3
jbfp <jakobp10@gmail.com>
2018-12-12 18:45:46 +0100

    Add Serilog

--- src/Sequence.Api/Program.cs
+++ src/Sequence.Api/Program.cs
@@ -2,6 +2,11 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Sequence.Sqlite;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+using Serilog.Sinks.SystemConsole.Themes;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -9,28 +14,55 @@ namespace Sequence.Api
 {
     public static class Program
     {
-        public static async Task Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            var builder = CreateWebHostBuilder(args);
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+                .WriteTo.File(new CompactJsonFormatter(), "./log.json")
+                .CreateLogger();
 
-            using (var host = builder.Build())
+            try
             {
-                using (var scope = host.Services.CreateScope())
-                {
-                    var sqlite = scope.ServiceProvider.GetService<SqliteConnectionFactory>();
+                Log.Information("Building web host");
+                var builder = CreateWebHostBuilder(args);
 
-                    if (sqlite != null)
+                using (var host = builder.Build())
+                {
+                    using (var scope = host.Services.CreateScope())
                     {
-                        await SqliteMigrations.UpgradeDatabaseAsync(sqlite, CancellationToken.None);
+                        var sqlite = scope.ServiceProvider.GetService<SqliteConnectionFactory>();
+
+                        if (sqlite != null)
+                        {
+                            Log.Information("Upgrading SQLite database if necessary");
+                            await SqliteMigrations.UpgradeDatabaseAsync(sqlite, CancellationToken.None);
+                        }
                     }
+
+                    Log.Information("Starting web host");
+                    await host.RunAsync(CancellationToken.None);
                 }
 
-                await host.RunAsync(CancellationToken.None);
+                Log.Information("Goodbye");
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
             }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) => WebHost
             .CreateDefaultBuilder(args)
-            .UseStartup<Startup>();
+            .UseStartup<Startup>()
+            .UseSerilog();
     }
 }
