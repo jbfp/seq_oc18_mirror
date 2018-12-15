0225a3a3e785a9d0c411353bbb8d0d5b7557e0b9
jbfp <jakobp10@gmail.com>
2018-12-15 10:52:45 +0100

    Update publish script

--- publish.sh
+++ publish.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+script_path="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
 set -e # break on error.
-cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" # cd to script location.
+cd $script_path # cd to script location.
 
 echo Building in Release mode...
 dotnet build -c Release -v q
@@ -19,19 +21,36 @@ cd ./wwwroot
 yarn run build
 cd ../
 
-echo Publishing...
+echo Publishing API...
 dotnet publish -c Release -v q
 cd ./bin/Release/netcoreapp2.2/publish/
 
-echo Killing remote server process...
-ssh jbfp@jbfp.dk pkill -x dotnet || true
+echo Killing remote server processes...
+ssh jbfp@jbfp.dk "pkill -x 'dotnet Sequence.Api.dll'" || true
 
 echo Copying files...
 git rev-parse HEAD > ./wwwroot/build/hash.txt
 rsync -ru --progress ./* jbfp@jbfp.dk:/home/jbfp/sequence
 
-echo Starting remote server process...
-ssh jbfp@jbfp.dk screen -S sequence -d -m "/home/jbfp/run.sh"
+echo Starting remote API process...
+ssh jbfp@jbfp.dk screen -S sequence -d -m "/home/jbfp/run-sequence.sh"
+
+cd $script_path
+
+echo Publishing Auth...
+cd ./src/Sequence.Auth/
+
+dotnet publish -c Release -v q
+cd ./bin/Release/netcoreapp2.2/publish/
+
+echo Killing remote server processes...
+ssh jbfp@jbfp.dk "pkill -x 'dotnet Sequence.Auth.dll'" || true
+
+echo Copying files...
+rsync -ru --progress ./* jbfp@jbfp.dk:/home/jbfp/auth
+
+echo Starting remote auth process...
+ssh jbfp@jbfp.dk screen -S auth -d -m "/home/jbfp/run-auth.sh"
 
 echo Done!
 
