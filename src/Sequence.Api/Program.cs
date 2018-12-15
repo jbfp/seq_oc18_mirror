4407a74597a65c7d85dc18019e8ac60d116243bb
jbfp <jakobp10@gmail.com>
2018-12-15 09:45:23 +0100

    Set fixed URL for API project

--- src/Sequence.Api/Program.cs
+++ src/Sequence.Api/Program.cs
@@ -63,6 +63,7 @@ namespace Sequence.Api
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) => WebHost
             .CreateDefaultBuilder(args)
             .UseStartup<Startup>()
-            .UseSerilog();
+            .UseSerilog()
+            .UseUrls("http://localhost:5000");
     }
 }
