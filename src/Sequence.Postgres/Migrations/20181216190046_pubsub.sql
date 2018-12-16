81b3ab214e13e9c88b723274c685b98f6997756d
jbfp <jakobp10@gmail.com>
2018-12-16 20:29:00 +0100

    Add NOTIFY trigger to game_event table on INSERT

---
+++ src/Sequence.Postgres/Migrations/20181216190046_pubsub.sql
@@ -0,0 +1,15 @@
+CREATE OR REPLACE FUNCTION notify_game_event_inserted()
+RETURNS trigger AS $$
+BEGIN
+    PERFORM pg_notify('game_event_inserted', row_to_json(NEW)::TEXT);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER game_event_inserted
+AFTER INSERT
+ON public.game_event
+FOR EACH ROW
+EXECUTE PROCEDURE notify_game_event_inserted();
+
+INSERT INTO public.migration (name) VALUES ('20181216190046_pubsub');
