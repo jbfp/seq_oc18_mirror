d6a8eb328e66a9614b5b72ba4546de0309c44a8e
jbfp <jakobp10@gmail.com>
2018-12-27 20:03:22 +0100

    Move Postgres projects into postgres folder

--- test.sh
+++ test.sh
@@ -3,3 +3,6 @@
 set -e # break on error.
 cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" # cd to script location.
 ls ./test/**/*.csproj | xargs -L1 -P 0 dotnet test -c Release -v q
+
+dotnet test -c Release -v q ./postgres/Sequence.Postgres.Test/
+
