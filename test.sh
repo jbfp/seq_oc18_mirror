e1a59f899d581c626fe9ad8d2d5016d5d908aeb0
jbfp <jakobp10@gmail.com>
2018-12-27 20:22:31 +0100

    Move Core projects into core folder

--- test.sh
+++ test.sh
@@ -2,9 +2,4 @@
 
 set -e # break on error.
 cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" # cd to script location.
-ls ./test/**/*.csproj | xargs -L1 -P 0 dotnet test -c Release -v q
-
-dotnet test -c Release -v q ./api/Sequence.Api.Test/
-dotnet test -c Release -v q ./auth/Sequence.Auth.Test/
-dotnet test -c Release -v q ./postgres/Sequence.Postgres.Test/
-
+ls ./**/*.Test/*.csproj | xargs -L1 -P 0 dotnet test -c Release -v q
