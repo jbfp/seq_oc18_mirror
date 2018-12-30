6f34137d0120cdf943ad29c60c822ccc6acf8cca
jbfp <jakobp10@gmail.com>
2018-12-30 22:10:53 +0100

    Add bot task and observer

---
+++ core/Sequence.Core/Bots/BotTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sequence.Core.Bots
+{
+    public sealed class BotTask
+    {
+        public BotTask(GameId gameId, PlayerHandle playerHandle)
+        {
+            GameId = gameId ?? throw new ArgumentNullException(nameof(gameId));
+            PlayerHandle = playerHandle ?? throw new ArgumentNullException(nameof(playerHandle));
+        }
+
+        public GameId GameId { get; }
+        public PlayerHandle PlayerHandle { get; }
+    }
+}
