682743520334fc19dbf95d9f473c70a14ea58578
jbfp <jakobp10@gmail.com>
2018-12-30 22:11:06 +0100

    Add bot task observer to core project

---
+++ core/Sequence.Core/Bots/BotTaskObserver.cs
@@ -0,0 +1,65 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sequence.Core.Bots
+{
+    public sealed class BotTaskObserver : BackgroundService, IObserver<BotTask>
+    {
+        private readonly BlockingCollection<BotTask> _tasks = new BlockingCollection<BotTask>();
+        private readonly IBotTaskObservable _observable;
+        private readonly ILogger _logger;
+
+        public BotTaskObserver(IBotTaskObservable observable, ILogger<BotTaskObserver> logger)
+        {
+            _observable = observable ?? throw new ArgumentNullException(nameof(observable));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            _tasks.Dispose();
+        }
+
+        public void OnCompleted()
+        {
+            _logger.LogInformation("No more bot tasks");
+            _tasks.CompleteAdding();
+        }
+
+        public void OnError(Exception error)
+        {
+            _logger.LogCritical(error, "Got error from bot task observable");
+            _tasks.CompleteAdding();
+        }
+
+        public void OnNext(BotTask task)
+        {
+            _logger.LogInformation(
+                "Received bot task for {PlayerId} in {GameId}",
+                task.GameId, task.PlayerHandle);
+
+            _tasks.Add(task);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            _logger.LogInformation("Subscribing to bot task observable");
+
+            using (_observable.Subscribe(this))
+            {
+                foreach (var task in _tasks.GetConsumingEnumerable(stoppingToken))
+                {
+                }
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
