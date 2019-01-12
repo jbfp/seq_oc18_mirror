43519c4f972cf5e83e8017fc1df51e10557d1b7b
jbfp <jakobp10@gmail.com>
2019-01-12 11:19:29 +0100

    Add number of sequences to win to NewGame

--- core/Sequence.Core.Test/CreateGame/NewGameTest.cs
+++ core/Sequence.Core.Test/CreateGame/NewGameTest.cs
@@ -5,15 +5,22 @@ namespace Sequence.Core.CreateGame.Test
 {
     public sealed class NewGameTest
     {
+        private static readonly PlayerList _players = new PlayerList(
+            new NewPlayer(
+                new PlayerHandle("test 1"),
+                PlayerType.User),
+            new NewPlayer(
+                    new PlayerHandle("test 2"),
+                    PlayerType.User));
+        private static readonly Seed _seed = new Seed(42);
+        private static readonly BoardType _boardType = BoardType.OneEyedJack;
+
         [Fact]
         public void NullArgs()
         {
-            var seed = new Seed(42);
-            var boardType = BoardType.OneEyedJack;
-
             Assert.Throws<ArgumentNullException>(
                 paramName: "players",
-                testCode: () => new NewGame(players: null, seed, boardType)
+                testCode: () => new NewGame(players: null, _seed, _boardType)
             );
         }
 
@@ -23,19 +30,25 @@ namespace Sequence.Core.CreateGame.Test
         [InlineData(100)]
         public void BoardTypeMustBeDefined(int boardType)
         {
-            var players = new PlayerList(
-                new NewPlayer(
-                    new PlayerHandle("test 1"),
-                    PlayerType.User),
-                new NewPlayer(
-                        new PlayerHandle("test 2"),
-                        PlayerType.User));
-
-            var seed = new Seed(42);
-
             Assert.Throws<ArgumentOutOfRangeException>(
                 paramName: "boardType",
-                testCode: () => new NewGame(players, seed, (BoardType)boardType)
+                testCode: () => new NewGame(_players, _seed, (BoardType)boardType)
+            );
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-100)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(42)]
+        [InlineData(100)]
+        public void NumSequencesToWinMustBeGreaterThanZeroAndLessThanFive(int numSequencesToWin)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                paramName: "numSequencesToWin",
+                testCode: () => new NewGame(_players, _seed, _boardType, numSequencesToWin)
             );
         }
     }
