77ab7ddfb8bbd499c9d554adc1ae7c047e7e1654
jbfp <jakobp10@gmail.com>
2019-02-09 14:53:01 +0700

    Update repository URL

--- api/Sequence.Api/wwwroot/src/Layout.jsx
+++ api/Sequence.Api/wwwroot/src/Layout.jsx
@@ -71,7 +71,7 @@ class Layout extends React.Component {
                     <span>By <a href="mailto:jakob@jbfp.dk">Jakob Pedersen</a></span>
                     <a href="https://www.linkedin.com/in/jakob-pedersen-835a824b" target="_blank" rel="noopener noreferrer">LinkedIn</a>
                     <a href="https://github.com/jbfp" target="_blank" rel="noopener noreferrer">GitHub</a>
-                    {!!hash ? <a href={`https://github.com/jbfp/seq_oc18_mirror/commit/${hash}`} target="_blank" rel="noopener noreferrer">{hash}</a> : null}
+                    {!!hash ? <a href={`https://github.com/jbfp/one_eyed_jack/commit/${hash}`} target="_blank" rel="noopener noreferrer">{hash}</a> : null}
                 </div>
             </div>
         )
