1bb6c85ebe2e92a90b6e9e6bcb6eb005087b1411
jbfp <jakobp10@gmail.com>
2019-02-09 14:25:50 +0700

    Add service worker

--- api/Sequence.Api/wwwroot/src/index.js
+++ api/Sequence.Api/wwwroot/src/index.js
@@ -5,6 +5,9 @@ import 'array.prototype.flat/auto';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as serviceWorker from './custom-service-worker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorker.register();
