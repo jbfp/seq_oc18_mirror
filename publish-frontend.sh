9ffdc687d0db718f4260f4eee5d4fd1e9bfbf7b0
jbfp <jakobp10@gmail.com>
2019-02-09 14:52:36 +0700

    Fix bug in frontend publish script

--- publish-frontend.sh
+++ publish-frontend.sh
@@ -8,7 +8,7 @@ cd ./api/Sequence.Api/wwwroot
 
 rm -rf ./build
 yarn run build
-git rev-parse HEAD > ./build/hash.txt-
+git rev-parse HEAD > ./build/hash.txt
 sed -i 's/assets.jbfp.dk\/index.html/www.jbfp.dk\/index.html/g' ./build/service-worker.js
 rsync -ru --progress --exclude="node_modules" ./* jbfp@jbfp.dk:/home/jbfp/sequence/wwwroot
 
