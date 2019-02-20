400945b2850ee7088ae8256fad99b22b49faa42d
jbfp <jakobp10@gmail.com>
2019-02-20 13:23:08 +0700

    Replace Card component with TypeScript impl

---
+++ api/Sequence.Api/wwwroot/src/Game/Card.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import * as t from "../types";
+
+interface CardProps {
+    card: t.Card;
+    isSelected: boolean;
+    onCardClick: (card: t.Card) => void;
+}
+
+export default function Card(props: CardProps) {
+    const { card, isSelected, onCardClick } = props;
+
+    return (
+        <div
+            className="card"
+            data-suit={card.suit}
+            data-rank={card.rank}
+            data-selected={isSelected}
+            onClick={() => onCardClick(card)}
+        ></div>
+    );
+}
+
