2c3847102e833d34d52df99b5e30fecb28e7e95a
jbfp <jakob@jbfp.dk>
2019-03-10 09:15:45 +0700

    Parallelize test execution of unit and integration tests

--- test.sh
+++ test.sh
@@ -2,4 +2,7 @@
 
 set -e # break on error.
 cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" # cd to script location.
-ls ./test/*.csproj | xargs -L1 -P 0 dotnet test -c Release -v q
+dotnet build --verbosity q --configuration Release
+dotnet test "test" -c Release --no-build --filter "Category=Integration" -v q &
+dotnet test "test" -c Release --no-build --filter "Category!=Integration" -v q &
+wait
