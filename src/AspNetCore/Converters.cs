f6409829f80bbc9a4e5e534c697d973acd9da48e
jbfp <jakob@jbfp.dk>
2019-03-12 21:27:36 +0700

    Optimize IGameEvent JSON converter

--- src/AspNetCore/Converters.cs
+++ src/AspNetCore/Converters.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json.Serialization;
 using Sequence.GetGame;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Reflection;
 
@@ -111,13 +112,51 @@ namespace Sequence.AspNetCore
 
     internal sealed class GameEventConverter : JsonConverter
     {
+        private static readonly ImmutableDictionary<Type, string> _keyByType;
+        private static readonly ImmutableDictionary<Type, PropertyInfo[]> _propertiesByType;
+
+        static GameEventConverter()
+        {
+            var gameEventTypes = typeof(IGameEvent).Assembly
+                .ExportedTypes
+                .Where(typeof(IGameEvent).IsAssignableFrom)
+                .ToList();
+
+            _keyByType = gameEventTypes
+                .ToImmutableDictionary(
+                    type => type,
+                    type => new string(type.Name
+                        .SelectMany(NameToChars)
+                        .ToArray()));
+
+            var bindingFlags = BindingFlags.Public | BindingFlags.Instance;
+
+            _propertiesByType = gameEventTypes
+                .ToImmutableDictionary(
+                    type => type,
+                    type => type.GetProperties(bindingFlags));
+        }
+
+        private static IEnumerable<char> NameToChars(char c)
+        {
+            if (char.IsUpper(c))
+            {
+                yield return '-';
+                yield return char.ToLowerInvariant(c);
+            }
+            else
+            {
+                yield return c;
+            }
+        }
+
         public override bool CanRead => false;
         public override bool CanWrite => true;
 
         public override bool CanConvert(Type objectType)
         {
-            return typeof(GameEventBase) != objectType
-                && typeof(GameEventBase).IsAssignableFrom(objectType);
+            return typeof(IGameEvent) != objectType
+                && typeof(IGameEvent).IsAssignableFrom(objectType);
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
@@ -128,16 +167,9 @@ namespace Sequence.AspNetCore
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             var type = value.GetType();
-            var chars = type.Name
-                .SelectMany(NameToChars)
-                .ToArray();
-
-            var name = new string(chars).Trim('-');
-
+            var name = _keyByType[type];
             var contractResolver = (DefaultContractResolver)serializer.ContractResolver;
-            var bindingFlags = BindingFlags.Public | BindingFlags.Instance;
-            var properties = type
-                .GetProperties(bindingFlags)
+            var properties = _propertiesByType[type]
                 .Select(p => (p.Name, Value: p.GetValue(value)))
                 .ToDictionary(
                     p => contractResolver.GetResolvedPropertyName(p.Name),
@@ -154,19 +186,6 @@ namespace Sequence.AspNetCore
             }
 
             writer.WriteEnd();
-        }
-
-        private static IEnumerable<char> NameToChars(char c)
-        {
-            if (char.IsUpper(c))
-            {
-                yield return '-';
-                yield return char.ToLowerInvariant(c);
-            }
-            else
-            {
-                yield return c;
-            }
         }
     }
 }
